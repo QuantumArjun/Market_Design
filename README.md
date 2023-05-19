3c60c4685afe1efaf8581ca5462365beafd076ee
Arjun Karanam <akaranam@stanford.edu>
2023-05-18 23:38:42 -0700

    Added some arg parse

--- README.md
+++ README.md
@@ -1,3 +1,40 @@
 # Market_Design
 
 Coding up some of the algorithms from Stanford's CS 136
+
+## Gale Shapley
+
+Input the preferences of the two sides under `preferences.json`
+
+Here is the format for the preferences: 
+```
+{
+    "left": {
+        "1": {
+            "ranking": "1, 2, 3"
+        },
+        "2": {
+            "ranking": "1, 2, 3"
+        },
+        "3": {
+            "ranking": "1, 2, 3"
+        }
+    },
+  
+    "right": {
+        "1": {
+            "ranking": "1, 2, 3"
+          },
+        "2": {
+            "ranking": "1, 2, 3"
+        },
+        "3": {
+            "ranking": "1, 2, 3"
+        }
+  }
+}
+```
+
+To run Deferred Acceptance (also known as Gale-Shapley), run the following command to run the left proposing DA algorithm:
+'python main.py --algorithm DA --proposing left'
+  
--- main.py
+++ main.py
@@ -7,12 +7,16 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--algorithm", help="The algorithm to use", choices=["DA"], default="DA")
     parser.add_argument("-p", "--proposing", help="The side of the market that proposes first", choices=["left", "right"], default="left")
     args = parser.parse_args()
 
     processor = Processor("preferences.json")
     preferences = processor.get_data()
-    gale_shapley = Gale_Shapley(preferences)
 
-    allocation = gale_shapley.match(type=args.proposing)
-    print(f"Under {args.proposing} proposing, the allocation is {allocation}")
+    algorithm = None
+    if args.algorithm == "DA":
+        algorithm = Gale_Shapley(preferences)
+
+    allocation = algorithm.match(type=args.proposing)
+    print(f"Using the {args.algorithm} algorithm, with {args.proposing} proposing, the allocation is {allocation}.\nNote, the allocation is given from the perspective of the matching side (i.e if {args.proposing} is proposing, the allocation is given from the perspective of the {args.proposing} side)")
